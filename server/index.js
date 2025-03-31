3dc7faeb13c9d4da99c3c032d20b92ea49059fec
Jack Redding <jredding2543@gmail.com>
2025-03-30 17:28:09 -0700

    Update cors on index.js

--- server/index.js
+++ server/index.js
@@ -4,28 +4,29 @@ const pool = require('./db');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://jredding43.github.io"
+}));
 app.use(express.json());
 
 app.post('/survey', async (req, res) => {
   const { answers } = req.body;
 
   try {
+    console.log("Received survey answers:", answers);
+
     await pool.query(
       'INSERT INTO survey_responses (answers) VALUES ($1)',
       [JSON.stringify(answers)]
     );
+
     res.status(200).send("Survey submitted successfully");
   } catch (err) {
     console.error(err);
     res.status(500).send("Error saving survey");
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
 });
 
 app.get('/responses', async (req, res) => {
@@ -38,3 +39,6 @@ app.get('/responses', async (req, res) => {
   }
 });
 
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
